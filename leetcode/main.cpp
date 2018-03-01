89cb64193241fa4841b2984a97fcab00311b1430
supiccc <supiccc@gmail.com>
2018-03-01 12:31:12 +0800

    init

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 陈树沛 on 01/03/2018.
+//  Copyright © 2018 supiccc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
